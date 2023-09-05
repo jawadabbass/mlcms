e4e8978e22b04b7667daa99c87d485601bbb0183
jawadabbass <jawadabbass@hotmail.com>
2023-09-05 15:21:49 +0500

    up

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 sudo chown -R $USER:www-data site_files
-sudo chmod -R 755 site_files
-sudo chmod -R 775 site_files/public/uploads
-sudo chmod -R 775 site_files/storage
+sudo chmod -R 444 site_files
+sudo chmod 755 $(sudo find site_files -type d)
+sudo chmod -R 774 site_files/public/uploads
+sudo chmod -R 774 site_files/storage
+sudo chmod -R 774 site_files/app/Http/Controllers
+sudo chmod -R 774 site_files/resources/views
