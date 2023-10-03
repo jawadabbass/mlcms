cd8461f4ac95b7130811115554d89e1497b98c41
jawadabbass <jawadabbass@hotmail.com>
2023-10-03 19:50:55 +0500

    nnn

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ sudo chown -R $USER:www-data site_files
 sudo chmod -R 774 $(sudo find site_files -type f)
 sudo chmod -R 775 $(sudo find site_files -type d)
 sudo chmod -R 644 $(sudo find site_files/storage/app/public -type f)
+#find . -type f -exec chmod 644 {} \;
