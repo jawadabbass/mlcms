8080f9888a10ce63fb3f2c38b1c5115dbe860c64
jawadabbass <jawadabbass@hotmail.com>
2024-05-23 15:41:02 +0500

    images webp

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo chown -R $USER:www-data site_files
 sudo chmod -R 644 site_files
 sudo chmod -R 755 $(sudo find site_files -type d)
+sudo chmod -R 775 site_files/db
 sudo chmod -R 775 site_files/public
 sudo chmod -R 775 site_files/storage/app/public/uploads
 sudo chmod -R 775 site_files/storage/logs
