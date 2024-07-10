abda0663d3e386809207c26addd5bdb241998484
jawadabbass <jawadabbass@hotmail.com>
2024-07-10 12:44:35 +0500

    chmod

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo chown -R $USER:www-data site_files
 sudo chmod -R 644 site_files
-sudo chmod -R 755 $(sudo find site_files -type d)
+find site_files -type d -exec sudo chmod 0755 {} \;
 sudo chmod -R 775 site_files/db
 sudo chmod -R 775 site_files/public
 sudo chmod -R 775 site_files/storage/app/public/uploads
