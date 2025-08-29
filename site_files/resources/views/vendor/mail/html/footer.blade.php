456c6d2db53cba80de322a2b247c976ae9ca82e4
jawadabbass <jawadabbass@hotmail.com>
2025-08-29 13:37:10 +0500

    upd

--- site_files/resources/views/vendor/mail/html/footer.blade.php
+++ site_files/resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,31 @@
-<tr>
-<td>
-<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
-</td>
-</tr>
+<table align="center" width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tbody>
+        <tr>
+            <td class="email_body email_end tc">
+                <table role="presentation" class="content_section" width="100%" border="0" cellspacing="0"
+                    cellpadding="0">
+                    <tbody>
+                        <tr>
+                            <td height="16">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table role="presentation" class="column" width="100%" border="0" cellspacing="0"
+                                    cellpadding="0" style="font-size: 14px">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center" valign="top" style="line-height: 20px">
+                                                <p>Copyright © {{ date('Y') }} {{ config('app.name') }}. All Rights
+                                                    Reserved</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
 </table>
-</td>
-</tr>
--- site_files/resources/views/vendor/mail/html/header.blade.php
+++ site_files/resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,9 @@
-@props(['url'])
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
-</tr>
+<table width="100%" border="0" align="center" cellpadding="10" cellspacing="0" bgcolor="#ffffff">
+    <tbody>
+        <tr>
+            <td align="center">
+                <img src="{!! asset_uploads('admin_logo_favicon/' . config('admin_logo_favicon.admin_login_page_logo')) !!}" />
+            </td>
+        </tr>
+    </tbody>
+</table>
